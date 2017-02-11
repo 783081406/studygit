ea948d77834f41a7de3c12646b7fb04a27f03b10
783081406 <783081406ccj@gmail.com>
2017-02-11 21:59:01 +0800

    使用分支修改的Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
 	public static void main(String[] args){
 		System.out.println("\u8fd9\u662f\u7b2c\u4e00\u884c\u4ee3\u7801\u505a\u7b2c\u4e00\u6b21\u63d0\u4ea4");
-		System.out.println("this is other peoper push")
+		System.out.println("this is other peoper push");
+		System.out.println("branch modify");
 	}
 }
